d6563af6970448b1d0c9de080076626997a07f8d
cskadam01 <adam.csokonyi@gmail.com>
2025-11-19 18:54:31 +0100

    Config

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from endpoints.users import router as users_router 
 from endpoints.groups import router as groups_router
 from endpoints.skills import router as skills_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+        allow_origins=[
+        "https://gymdatabase-129e2.web.app",
+        "http://localhost:5173"
+    ],
+  # vagy a saját domained
+    allow_credentials=True,  # ⬅⬅⬅ FONTOS: kell a sütikhez!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(groups_router, prefix="/groups", tags=["Groups"])
